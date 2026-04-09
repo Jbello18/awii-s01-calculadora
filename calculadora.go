1fdc89f63671b7b7f60505e31b90774b4640a11b
John Bello Alava <bellojhon88@gmail.com>
2026-04-09 18:02:25 -0500

    Completado Paso 1 y 2 - Operaciones básicas

--- calculadora.go
+++ calculadora.go
@@ -3,6 +3,22 @@ package main
 import "fmt"
 
 func main(){
-	
+	//Encabezado decorativo
+	fmt.Println("==== CALCULADORA CIENTÍFICA v1.0 ====")
+
+	//Variables fijas
+	a := 14
+	b := 62
+
+	//Operaciones
+	suma := a + b
+	resta := a - b
+	multiplicacion := a * b 
+	division := float64(a) / float64(b)
+
+	fmt.Printf("a + b = %d\n", suma)
+	fmt.Printf("a - b = %d\n", resta)
+	fmt.Printf("a * b = %d\n", multiplicacion)
+	fmt.Printf("a / b = %.2f\n", division)
 
 }
