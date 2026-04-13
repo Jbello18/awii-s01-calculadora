d3ac33d12c83c6e7ec3339b1b9d2420285b5d12d
John Bello Alava <bellojhon88@gmail.com>
2026-04-13 12:15:50 -0500

    Completado el check point 2: switch, potencia y factorial

--- calculadora.go
+++ calculadora.go
@@ -2,23 +2,54 @@ package main
 
 import "fmt"
 
-func main(){
-	//Encabezado decorativo
+func main() {
+	// Encabezado
 	fmt.Println("==== CALCULADORA CIENTÍFICA v1.0 ====")
 
-	//Variables fijas
-	a := 14
-	b := 62
+	var n1 float64
+	var n2 float64
+	var operacion string
 
-	//Operaciones
-	suma := a + b
-	resta := a - b
-	multiplicacion := a * b 
-	division := float64(a) / float64(b)
+	// 1. Pedir los datos al usuario
+	fmt.Print("Ingresa el primer número: ")
+	fmt.Scan(&n1)
+	fmt.Print("Ingresa el segundo número: ")
+	fmt.Scan(&n2)
 
-	fmt.Printf("a + b = %d\n", suma)
-	fmt.Printf("a - b = %d\n", resta)
-	fmt.Printf("a * b = %d\n", multiplicacion)
-	fmt.Printf("a / b = %.2f\n", division)
+	fmt.Print("Ingresa la operación (+, -, *, /, ^, !): ")
+	fmt.Scan(&operacion)
 
+	// 3.
+	switch operacion {
+	case "+":
+		fmt.Printf("Resultado: %.2f + %.2f = %.2f\n", n1, n2, n1+n2)
+	case "-":
+		fmt.Printf("Resultado: %.2f - %.2f = %.2f\n", n1, n2, n1-n2)
+	case "*":
+		fmt.Printf("Resultado: %.2f * %.2f = %.2f\n", n1, n2, n1*n2)
+	case "/":
+		if n2 == 0 {
+			fmt.Println("Error: no se puede dividir entre cero")
+		} else {
+			fmt.Printf("Resultado: %.2f / %.2f = %.2f\n", n1, n2, n1/n2)
+		}
+	case "^":
+		resultadoPot := 1.0
+		for i := 0; i < int(n2); i++ {
+			resultadoPot *= n1
+		}
+		fmt.Printf("Resultado: %.2f ^ %.0f = %.2f\n", n1, n2, resultadoPot)
+	case "!":
+		if n1 < 0 {
+			fmt.Println("Error: No existe factorial de números negativos.")
+		} else {
+			resultadoFact := 1.0
+			for i := 1; i <= int(n1); i++ {
+				resultadoFact *= float64(i)
+			}
+			fmt.Printf("Resultado: %.0f! = %.2f\n", n1, resultadoFact)
+		}
+	default:
+		fmt.Println("Error: operación no reconocida")
+	}
 }
